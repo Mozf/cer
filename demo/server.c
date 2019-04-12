d847c9005c303be80f57459a9655bd85fd904750
Mozf <1345238761@qq.com>
2019-04-12 13:35:05 +0800

    mo

--- demo/server.c
+++ demo/server.c
@@ -6,7 +6,6 @@
 #include <netinet/ip.h> /* superset of previous */
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <arpa/inet.h>
 
 struct Data{
     int len;
@@ -20,8 +19,8 @@ int main()
     //* bind
     struct sockaddr_in6 server_addr;
     server_addr.sin6_family = AF_INET6;
-    server_addr.sin6_port = htons(5670);
-    if (inet_pton(AF_INET6,"2001:da8:270:2018:f816:3eff:fe40:d788", &server_addr.sin6_addr) < 0 )
+    server_addr.sin6_port = htons(8088);
+    if (inet_pton(AF_INET6,"2001:da8:270:2018:f816:3eff:fe40:d788", &server_addr.sin6_addr) < 0 ) 
     {                 // IPv6
         perror("inet_pton err");
         return -1;
@@ -48,6 +47,18 @@ int main()
             printf("new_sockfd = %d\n",new_sockfd);
             //2 recv
             struct Data data;
+            struct Data cli_to_ser;
+            int res_clitoser=recv(new_sockfd,&cli_to_ser,sizeof(cli_to_ser),0);
+            if(res_clitoser<0)
+            {
+                perror("recv file from client err:");
+                return -1;
+            }
+            else
+            {
+                printf("recv buff from client success\n");
+                printf("recv buff is %s\n",cli_to_ser.buf);
+            }
             int res = recv(new_sockfd,&data,sizeof(data),0);
             if(res < 0)
             {
