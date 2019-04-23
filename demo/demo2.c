4038fa4209d446df6b5a15ee3a079e330b3b95fb
Mozf <1345238761@qq.com>
2019-04-23 14:08:23 +0800

    mo

--- demo/demo2.c
+++ demo/demo2.c
@@ -4,7 +4,6 @@
 #include <ctype.h>
 #include "engine.h"
 #include "matrix.h"
-#include	<time.h>
 #include <sys/types.h>   
 #include <sys/socket.h>
 #include <unistd.h>
@@ -16,131 +15,141 @@
 int main(int argc, char **argv)
 {
 
-	//matlab define =========================================================
-	Engine *ep;
-	char level[5],buffer[BUFSIZE + 1];
-	int i =0, j =0,flag =0;
-	mxArray *testdata = NULL, *result = NULL;
+  //matlab define =========================================================
+  Engine *ep;
+  char level[5],buffer[BUFSIZE + 1];
+  int i =0, j =0,flag =0;
+  mxArray *testdata = NULL, *result = NULL;
 
-	//socket define =========================================================
-	int	        listenfd, connfd;
-	char 				buff_1[MAXLINE + 1], buff_2[MAXLINE + 1] ;
-	time_t			ticks;
-	socklen_t		len;
-	
-	struct sockaddr_in6	servaddr, cliaddr;
 
-	//socket ================================================================
-	if ((listenfd = socket(AF_INET6, SOCK_STREAM, 0)) == -1) {
+  //socket define =========================================================
+  int	        listenfd, connfd;
+  char 				buff_1[MAXLINE + 1], buff_2[MAXLINE + 1] ;
+  time_t			ticks;
+  socklen_t		len;
+  
+  struct sockaddr_in6	servaddr, cliaddr;
+
+
+  //socket ================================================================
+  if ((listenfd = socket(AF_INET6, SOCK_STREAM, 0)) == -1) {
     perror("socket failed!\n");
-		exit(1);
+    exit(1);
   }
-	else {
-		printf("socket created\n");
-	}
+  else {
+    printf("socket created\n");
+  }
 
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin6_family = AF_INET6;
-	servaddr.sin6_addr   = in6addr_any;
-	servaddr.sin6_port   = htons(5670);	
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin6_family = AF_INET6;
+  servaddr.sin6_addr   = in6addr_any;
+  servaddr.sin6_port   = htons(5670);	
 
-	if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) == -1) {
+  if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) == -1) {
     perror("bind failed!\n");
-		exit(1);
+    exit(1);
   }
-	else {
-		printf("binded\n");
-	}
+  else {
+    printf("binded\n");
+  }
 
-	if (listen(listenfd, 5) == -1) {
+  if (listen(listenfd, 5) == -1) {
     perror("listen failed!\n");
-		exit(1);
+    exit(1);
   }
-	else {
-		printf("begin listen\n");
-	}
-	//matlab======================================================================
-	if (!(ep =engOpen("/work/matlab2016/bin/matlab")))
-	{
-		printf("\nCan't start MATLAB engine!\n\n");
-		return EXIT_FAILURE;
-	}	
-	engSetVisible(ep,false);
-	//=============================================================================
+  else {
+    printf("begin listen\n");
+  }
 
-	//socket=======================================================================
-	len = sizeof(cliaddr);
 
-	if ((connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len)) == -1) {
-		perror("accept failed!\n");
-		exit(1);
-	}
-	else {
-		printf("accept\n");
-	}
+  //matlab======================================================================
+  if (!(ep =engOpen("/work/matlab2016/bin/matlab")))
+  {
+    printf("\nCan't start MATLAB engine!\n\n");
+    return EXIT_FAILURE;
+  }	
+  engSetVisible(ep,false);
 
-	if(len = recv(connfd,buff_1,sizeof(buff_1),0) == -1) {
-		perror("recv failed!\n");
-	}
-	else {
-		printf("%s\n",buff_1);
-	}
-	
-	char data[241]={0};
-	strncpy(data,buff_1,240);
-	//=============================================================================
 
-	//服务器做数据截取，得气味数据120*2
-	//char data[]= {"022936010229360102293601032936010229360102293601022836010328350103283500022734010227340102263301032633010325320003243101022431010223300103222F0103222E0103222E0102212D0102212D0102222C0103222C0002222C0102222C0102222C0102222C0103222C0102212B01"};
+  //socket=======================================================================
+  while(1) {
+    len = sizeof(cliaddr);
 
-	//	data[240]='/0';
-	testdata = mxCreateString(data); //mxCreateString创建一个1*N维的字符串阵列。
+    if ((connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len)) == -1) {
+      perror("accept failed!\n");
+      exit(1);
+    }
+    else {
+      printf("accept\n");
+    }
 
-	if(flag != engPutVariable(ep, "input_data", testdata))			//检查发数据情况
-	{
-		printf("f2ff\n");			
-	}
+    memset(buff_1, 0, sizeof(buff_1));
+    if(len = recv(connfd,buff_1,sizeof(buff_1),0) == -1) {
+      perror("recv failed!\n");
+    }
+    else {
+      printf("%s\n",buff_1);
+    }
+    
+    char data[241]={0};
+    strncpy(data,buff_1,240);
+    
 
-	if(flag != engEvalString(ep, "save ('data.mat','input_data');"))	//检查存数据情况
-	{
-		printf("f3ff\n"); 
-	}
+    //matlab=============================================================================
+    //服务器做数据截取，得气味数据120*2
+    //char data[]= {"022936010229360102293601032936010229360102293601022836010328350103283500022734010227340102263301032633010325320003243101022431010223300103222F0103222E0103222E0102212D0102212D0102222C0103222C0002222C0102222C0102222C0102222C0103222C0102212B01"};
 
-	buffer[BUFSIZE] = '\0';
-	engOutputBuffer(ep, buffer, BUFSIZE);						
-	if(flag != engEvalString(ep, "Mtest"))					 //检查算法文件执行
-	{
-		printf("f4ff\n");
-	}
+    //	data[240]='/0';
+    testdata = mxCreateString(data); //mxCreateString创建一个1*N维的字符串阵列。
 
-	if ((result = engGetVariable(ep, "level")) !=NULL) {						
-	//	   printf("the right buffer is  %s\n",buffer);
-		for (i = 0; i < 2; i++) {
-			level[i] = buffer[i + 14];					//kinds:0x01,0x02,0x03
-			level[i+2] = buffer[i+30];					//levels:0x01,0x02,0x03,0x04,0x05
-		}
-		level[4]='\0';
-		printf("level:%s\n", level);					//输出结果：第一字节是分类结果，第二字节是分级结果
-	}else
-			printf("ffff\n" );
+    if(flag != engPutVariable(ep, "input_data", testdata))			//检查发数据情况
+    {
+      printf("f2ff\n");			
+    }
 
-//socket=================================================================================
-	memset(buff_2, 0, sizeof(buff_2));
-	strcpy(buff_2, level);
-	if (len = send(connfd, buff_2, sizeof(buff_2), 0) == -1) {
-		perror("send failed!\n");
-	}
-	else {
-		printf("send\n");
-	}
-	close(connfd);
-//socket=================================================================================
+    if(flag != engEvalString(ep, "save ('data.mat','input_data');"))	//检查存数据情况
+    {
+      printf("f3ff\n"); 
+    }
 
-	mxDestroyArray(testdata);					
-	mxDestroyArray(result);									
+    buffer[BUFSIZE] = '\0';
+    engOutputBuffer(ep, buffer, BUFSIZE);						
+    if(flag != engEvalString(ep, "Mtest"))					 //检查算法文件执行
+    {
+      printf("f4ff\n");
+    }
 
-	engClose(ep);		
-	return EXIT_SUCCESS;
+    if ((result = engGetVariable(ep, "level")) !=NULL) {						
+    //	   printf("the right buffer is  %s\n",buffer);
+      for (i = 0; i < 2; i++) {
+        level[i] = buffer[i + 14];					//kinds:0x01,0x02,0x03
+        level[i+2] = buffer[i+30];					//levels:0x01,0x02,0x03,0x04,0x05
+      }
+      level[4]='\0';
+      printf("level:%s\n", level);					//输出结果：第一字节是分类结果，第二字节是分级结果
+    }else
+        printf("ffff\n" );
+
+
+    //socket=================================================================================
+    memset(buff_2, 0, sizeof(buff_2));
+    strcpy(buff_2, level);
+    if (len = send(connfd, buff_2, sizeof(buff_2), 0) == -1) {
+      perror("send failed!\n");
+    }
+    else {
+      printf("send\n");
+    }
+    close(connfd);
+
+
+    //matlab=================================================================================
+    mxDestroyArray(testdata);					
+    mxDestroyArray(result);									
+  }
+
+  engClose(ep);		
+  return EXIT_SUCCESS;
 }
 
 
