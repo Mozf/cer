7178d78a316e959825f9ff82a32859831e99b9aa
Mozf <1345238761@qq.com>
2019-12-18 20:25:45 +0800

    update socketser.py

--- python/serial/socketser.py
+++ python/serial/socketser.py
@@ -5,10 +5,10 @@ from time import ctime
 ServerSocket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 print("Done.....1")
 
-host = socket.gethostname()
+host = "2001:da8:270:2018:f816:3eff:fe40:d788"
 print('my host : {}'.format(host))
 
-port = 8088
+port = 5670
 
 ServerSocket.bind((host, port))#2001:da8:270:2018:f816:3eff:fe40:d788
 
@@ -16,7 +16,7 @@ ServerSocket.listen(1)
 
 while True:
   clientsocket, addr = ServerSocket.accept()
-  print("连接地址:%s" % str(addr))
+  print("addr is :%s" % str(addr))
   msg = "welcomt to my demo"
   clientsocket.send(msg.encode("utf-8"))
   data = clientsocket.recv(1024)
