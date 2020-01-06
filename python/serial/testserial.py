81c0f0299f981f9bab8880caa08dbc43a22298a8
Mozf <1345238761@qq.com>
2020-01-06 14:36:25 +0800

    update testserial.py

--- python/serial/testserial.py
+++ python/serial/testserial.py
@@ -1,13 +1,12 @@
 import time
 import serial
 
-ser = serial.Serial(
-    port='COM4',
-     baudrate=115200
-)
+ser = serial.Serial(port='COM4', baudrate=115200)
 
 data = ''
-datafromuart = '1111\r\n'
+datafromuart = 'ggg'
+print(data)
+print(datafromuart)
 
 while True:
     data = ser.readline()
@@ -15,7 +14,11 @@ while True:
     ct = time.ctime(t)
     print(ct, ':')
     print(data)
+    data = data.decode()
+    data1 = data[0:3]
+    print(data1)
 
-    while (data == datafromuart):
-      ser.write('g\r\n'.encode())
-      print("send success!")
+    if (data[0:3] == datafromuart):
+         print(1)
+         ser.write('g\r\n'.encode())
+         print("send successful!")
