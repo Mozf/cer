087d0f3aff9dac95486969493b5d12ebcd1d529b
Mozf <1345238761@qq.com>
2020-07-05 14:04:45 +0800

    modified run

--- python/run.py
+++ python/run.py
@@ -6,30 +6,24 @@ from model import app
 import time
 
 ServerSocket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-ServerSocket1 = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 print("Done.....1")
 
 host = "2001:da8:270:2018:f816:3eff:fe40:d788"
 print('my host : {}'.format(host))
 
 port = 5670
-port1 = 8088
 
 ServerSocket.bind((host, port))#2001:da8:270:2018:f816:3eff:fe40:d788
-ServerSocket1.bind((host, port1))
 
 ServerSocket.listen(1)
-ServerSocket1.listen(1)
 msg = 'awaite'
 flag = 0
 
 while True:
   clientsocket, addr = ServerSocket.accept()
   # print("addr is :%s" % str(addr))
-  clientsocket1, addr1 = ServerSocket1.accept()
 
   name = clientsocket.recv(1024)
-  data = clientsocket1.recv(1024)
 
   #send back============================================================================================================
 
@@ -42,7 +36,7 @@ while True:
       print('the odour is '+ msg.decode()[1:])
     print('Send successfully')
 
-  elif name.decode("utf-8") == 'get':
+  if name.decode("utf-8") == 'get':
     if flag == 0:
       clientsocket.send(msg.encode())
     else:
@@ -56,18 +50,13 @@ while True:
     flag = 0
     msg = 'awaite'
 
-  elif name.decode("utf-8").isdigit():
+  if name.decode("utf-8").isdigit():
     pass
-
   else:
     clientsocket.send(name)
     flag = 1
     msg = name
 
   clientsocket.close()
-
-  if data.decode("utf-8").isdigit():
-    print(data)
-    clientsocket1.close()
   
 ServerSocket.close()
